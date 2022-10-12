96387bc7314f53d84e62d87a18689ee8acee6448
Gardn <gardn769@gmail.com>
2022-10-12 16:52:29 +0300

    +fix auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,20 +3,21 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
 import { Model } from 'mongoose';
 import { SignupDto } from './dto/signup.dto';
-import { UtilService } from '../common/utils/util.service';
 import { SigninDto } from './dto/signin.dto';
+import { createHash } from 'crypto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name)
     private userModel: Model<UserDocument>,
-    private utilService: UtilService,
+    private jwtService: JwtService,
   ) {}
 
   signup(data: SignupDto): Promise<Partial<User>> {
     const user: UserDocument = new this.userModel({
-      passwordHash: this.utilService.getHash(data.password),
+      passwordHash: createHash('md5').update(data.password).digest('hex'),
       email: data.email,
       firstName: data.firstName,
       lastName: data.lastName,
@@ -33,14 +34,19 @@ export class AuthService {
         email: data.email,
       })
       .exec();
-    const passwordHash: string = this.utilService.getHash(data.password);
+    const passwordHash: string = createHash('md5')
+      .update(data.password)
+      .digest('hex');
     if (!user) {
       throw new UnauthorizedException();
     }
     if (passwordHash !== user.passwordHash) {
       throw new UnauthorizedException();
     }
+    console.log(this.jwtService.sign(data));
+
     delete user.passwordHash;
+
     return user;
   }
 }
