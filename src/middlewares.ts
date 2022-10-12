1ef737d8cf7b8340704e272b1c83ceb861ef772d
Gardn <gardn769@gmail.com>
2022-10-12 13:31:53 +0300

    +pipes, interceptors and filters in module +class validator in dto

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,5 +1,15 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 export function middlewares(app: INestApplication): INestApplication {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      enableDebugMessages: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
   return app;
 }
